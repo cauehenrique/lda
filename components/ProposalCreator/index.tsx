638c887a42c239bdd51c86cd9e82f0641645b881
cauehenrique <caue313@gmail.com>
2021-09-19 21:49:17 -0300

    Added: Confirmation modal

--- components/ProposalCreator/index.tsx
+++ components/ProposalCreator/index.tsx
@@ -1,19 +1,74 @@
-import { HandIcon } from "@heroicons/react/outline";
-import { FC } from "react";
+import { FC, useState } from "react";
 
-import { Container, Area, SubmitContainer, SubmitButton } from "./styles";
+import {
+  Container,
+  Area,
+  SubmitButton,
+  ModalContainer,
+  ModalContent,
+  RMInput,
+  ModalButton,
+} from "./styles";
+
+import { CheckCircleIcon, XIcon } from "@heroicons/react/solid";
+import api from "~/services/api";
+
+interface Post {
+  titulo: string;
+  conteudo: string;
+  autor: string;
+}
 
 const ProposalCreator: FC = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<Post>();
+
+  async function submitProposal() {
+    const apiURL = `/propostas/publicar?titulo=${post.titulo}&conteudo=${post.conteudo}&autor=${post.autor}`;
+    // await api.get()
+    setModalOpen(false);
+  }
+
   return (
-    <Container
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <Area placeholder="Qual a sua ideia?"></Area>
-      <SubmitButton type="submit">
-        <HandIcon className="block w-5 h-5" />
+    <Container>
+      <Area placeholder="Qual a sua ideia?" maxLength={120}></Area>
+      <SubmitButton onClick={() => setModalOpen(true)}>
+        <CheckCircleIcon className="block w-5 h-5" />
       </SubmitButton>
+
+      <ModalContainer $isVisible={modalOpen}>
+        <ModalContent>
+          <h1 className="text-2xl font-semibold mb-1">Atenção</h1>
+          <h2>
+            Contamos com seu bom senso na hora de sugerir projetos para a nossa
+            escola.
+            <br />
+            <br />
+            Para evitar mensagens indevidas, precisamos do seu RM para
+            identificá-lo em nossa base de dados.
+          </h2>
+          <div className="mt-6 flex flex-col">
+            <span className="w-min block font-semibold bg-white dark:bg-gray-800 px-0.5 ml-2 -mb-3 z-10">
+              RM
+            </span>
+            <RMInput
+              type="text"
+              placeholder="00000"
+              maxLength={5}
+              onChange={(e) => setPost({ ...post, autor: e.target.value })}
+            />
+            <ModalButton onClick={() => submitProposal()}>
+              <CheckCircleIcon className="w-5 h-5" />
+            </ModalButton>
+          </div>
+        </ModalContent>
+        <button
+          className="block absolute top-8 right-8 text-white p-2 bg-black bg-opacity-50 rounded-full"
+          onClick={() => setModalOpen(false)}
+        >
+          <XIcon className="w-5 h-5" />
+        </button>
+      </ModalContainer>
     </Container>
   );
 };
--- components/ProposalCreator/styles.ts
+++ components/ProposalCreator/styles.ts
@@ -1,12 +1,9 @@
 import tw from "tailwind-styled-components";
 
-export const Container = tw.form`
+export const Container = tw.div`
 flex flex-col
 mb-4
 overflow-hidden
-border-2
-border-gray-300 dark:border-gray-600
-focus-within:border-indigo-600 dark:focus-within:border-indigo-600
 text-black dark:text-white
 rounded-sm
 `;
@@ -16,12 +13,11 @@ w-full h-24
 p-4
 outline-none
 resize-none
-bg-white
-dark:bg-gray-800
-`;
-
-export const SubmitContainer = tw.div`
-flex
+border-2
+border-b-0
+border-gray-300 dark:border-gray-600
+focus:border-indigo-600 dark:focus:border-indigo-600
+bg-white dark:bg-gray-800
 `;
 
 export const SubmitButton = tw.button`
@@ -31,4 +27,56 @@ flex justify-center items-center
 transition-colors duration-75
 bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600
 text-white dark:text-white
+`;
+
+type ModalContainer = {
+  $isVisible: boolean;
+};
+
+export const ModalContainer = tw.div<ModalContainer>`
+w-full min-h-screen
+fixed
+top-0 left-0
+bg-black
+bg-opacity-60
+
+flex
+items-center
+justify-center
+
+z-20
+
+${({ $isVisible }) => !$isVisible && "hidden"}
+`;
+
+export const ModalContent = tw.main`
+p-8
+max-w-xl
+bg-white dark:bg-gray-800
+rounded-sm
+`;
+
+export const RMInput = tw.input`
+p-2
+w-full
+block
+outline-none
+border-2
+border-b-0
+border-gray-300 focus:border-indigo-600
+dark:border-gray-700 dark:focus:border-indigo-600
+bg-white dark:bg-gray-800
+rounded-sm
+rounded-b-none
+`;
+
+export const ModalButton = tw.button`
+p-2
+flex
+items-center
+justify-center
+rounded-sm
+rounded-t-none
+bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600
+text-white
 `;
